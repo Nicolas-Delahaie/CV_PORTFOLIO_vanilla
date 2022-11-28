e937579506f3950bb435df68454268bfa4e0bba4
Nicolas Delahaie <nico601.delahaie@gmail.com>
2022-11-28 23:28:41 +0100

    Ajout de la fonction de centrage d un element (non utilise)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var dimensionsImages = new Map();
+var defaultDimensions = new Map();
 var idDerniereImage = null;
 
 
+
 function clickImage(event){
+    event.stopPropagation();
     if (!event.target.parentElement.classList.contains("agrandie"))
     {
         //Taille d image par defaut --> On l agrandie
@@ -22,9 +24,11 @@ function majAncienAgrandissement()
     {
         //Remise des dimensions par defaut
         var derniereImage = document.getElementById(idDerniereImage);
-        
-        derniereImage.style.width = dimensionsImages[idDerniereImage][0];
-        derniereImage.style.height = dimensionsImages[idDerniereImage][1];
+        derniereImage.style.width = defaultDimensions[idDerniereImage][0];
+        derniereImage.style.height = defaultDimensions[idDerniereImage][1];
+
+        //Suppression du centrage
+        // decentrerElement(derniereImage);
         
         //Suppresion de la classe agrandie
         derniereImage.classList.remove("agrandie");
@@ -33,14 +37,15 @@ function majAncienAgrandissement()
 function agrandissementImage(event)
 {
     //Sauvegarde des dimensions avant modification
-    dimensionsImages[event.target.parentElement.getAttribute("id")] =  [event.target.parentElement.clientWidth, event.target.parentElement.clientHeight];
+    defaultDimensions[event.target.parentElement.getAttribute("id")] =  [event.target.parentElement.clientWidth, event.target.parentElement.clientHeight];
     //Agrandit l image le plus grand possible 
     imgWidth = event.target.parentElement.clientWidth; 
     imgHeight = event.target.parentElement.clientHeight; 
     coefHeight = imgHeight / imgWidth           //Multiplier la largeur pour avoir la longueur
 
-    widthLimit = window.innerWidth * 0.7;       //Largeur maximum de l image 
-    heightLimit = window.innerHeight * 0.8;     //longueur maximum de l image 
+    // alert("Largeur avant traitement : "+imgWidth);
+    widthLimit = window.innerWidth * 0.5;       //Largeur maximum de l image 
+    heightLimit = window.innerHeight * 0.9;     //longueur maximum de l image 
 
     //Essai d agrandissement de la largeur a la taille de la fenetre
     imgWidth = widthLimit;
@@ -51,13 +56,17 @@ function agrandissementImage(event)
         imgHeight = heightLimit;                //Mise par defaut de la longueur
         imgWidth = imgHeight*(1/coefHeight);    //Calcul de la nouvelle largeur
     }
+    
     //Assignation des nouvelles largeurs et longueurs
     event.target.parentElement.style.width = imgWidth;
     event.target.parentElement.style.height = imgHeight;
 
+    //Centrage de l element
+    // centrerElement(event.target.parentElement);
+
     //Repositionnement fenetre centree par rapport a l image
     yImage = event.target.parentElement.offsetTop;
-    newY =    yImage -(window.innerHeight-imgHeight) / 2;
+    newY = yImage -(window.innerHeight-imgHeight) / 2;
     window.scrollTo(0, newY);
     
     //Ajout de la classe agrandie
@@ -70,25 +79,40 @@ function majParDefaut(event)
 {
     //Remise des dimensions par defaut
     idElement = event.target.parentElement.getAttribute("id");
-    event.target.parentElement.style.width = dimensionsImages[idElement][0];
-    event.target.parentElement.style.height = dimensionsImages[idElement][1];
+    event.target.parentElement.style.width = defaultDimensions[idElement][0];
+    event.target.parentElement.style.height = defaultDimensions[idElement][1];
+
+    //Decentrage de l element
+    decentrerElement(event.target.parentElement);
 
     //Repositionnement fenetre centree par rapport a l image
     yImage = event.target.parentElement.offsetTop;
     newY = yImage - (window.innerHeight - event.target.parentElement.offsetHeight) / 2;
     window.scrollTo(0, newY);
-    
+
     //Suppresion de la classe agrandie
     event.target.parentElement.classList.remove("agrandie");
 
     //Maj de l id de la derniere image modifiee
     idDerniereImage = null;
 }
+function centrerElement(elmt)
+{
+    var marge = (window.innerWidth - elmt.offsetWidth) / 2;
+    // alert("Fentre : "+window.innerWidth+"px\nElement :"+elmt.offsetWidth+"  "+elmt.style.width+"px\nMarge : "+marge+"px");
+    elmt.style.margin = "20px "+marge+"px";
+
+}
+function decentrerElement(elmt)
+{
+    elmt.setAttribute("style", "margin: 20px");
+}
 
 photos = document.getElementsByClassName("photosProjets");
 for (let index = 0; index < photos.length; index++) {
     photos[index].addEventListener('click', clickImage);
 }
+document.getElementById("zonePhotosProjets").addEventListener('click', majAncienAgrandissement);
 // document.querySelectorAll(".photosProjets").array.forEach(element => element.addEventListener('click', test));
 
 
